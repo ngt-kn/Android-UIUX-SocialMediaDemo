89c51f9963af36b5141ea21a4b2bcea15d279c0c
ngt kn <nagata.ken78@gmail.com>
2018-07-04 18:26:05 -0700

    add posts and friend profile

--- app/src/main/java/com/ngtkn/socialmediaprofile/MainActivity.java
+++ app/src/main/java/com/ngtkn/socialmediaprofile/MainActivity.java
@@ -1,13 +1,26 @@
 package com.ngtkn.socialmediaprofile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.friend6);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FriendActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
